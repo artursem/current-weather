8c5152451ff2f28e1a0cec9483e9fad0c7efc397
artursem <artur.semeniuk@gmail.com>
2021-11-21 18:20:08 +0100

    add icon

--- src/Components/DisplayWeather.js
+++ src/Components/DisplayWeather.js
@@ -6,41 +6,58 @@ function DisplayWeather(props) {
 	const [error, setError] = useState(null);
 
 	const fetchWeather = async (query) => {
+		setError(null)
 		setLoading(true);
 		const appid = "599f9ab00f5ffd6eeb1a6bf54606a714";
 		try {
 			const response = await fetch(
 				`http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appid}`
-				);
-	
-				if (!response.ok) {
-					setError(true)
-					throw new Error('erororororo!')
-				}
-				const data = await response.json();
-				console.log(data);
-	
-				setDisplay({
-					city: data.name,
-					weather: data.weather[0].main,
-					temp: `${(data.main.temp-272).toFixed(0)}°C`,
-				});
+			);
 
+			if (!response.ok) {
+				setLoading(false);
+				setError(error.message);
+				throw new Error("No valid response");
+			}
+			const data = await response.json();
+			console.log(data);
+
+			setLoading(false);
+			setDisplay({
+				city: data.name,
+				weather: data.weather[0].main,
+				temp: `${(data.main.temp - 272).toFixed(0)}°C`,
+				icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+			});
 		} catch (error) {
 			console.log(error);
 		}
+	};
 
-		};
+	useEffect(() => fetchWeather(props.get), [props.get]);
 
-		useEffect(() => fetchWeather(props.get), [props.get])
+	let show;
 
-	return (
-		<div>
-			<div>{display.city}</div>
-			<div>{display.weather}</div>
-			<div>{display.temp}</div>
-		</div>
-	);
+	if (loading) {
+		show = <p>Loading...</p>;
+	}
+	if (!loading && !error) {
+		show = (
+			<section>
+				<h1>{display.city}</h1>
+				<div>{display.weather}</div>
+				<div>{display.temp}</div>
+				<div>
+					<img src={display.icon} alt={display.weather} />{" "}
+				</div>
+			</section>
+		);
+	}
+	if (error) {
+		show = <p>Something went wrong...</p>
+	}
+
+	return show;
 }
 
 export default DisplayWeather;
