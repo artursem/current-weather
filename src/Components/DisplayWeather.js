a57ad1b692cead2f83ad9a2320ecb4293010a8c2
artursem <artur.semeniuk@gmail.com>
2021-11-21 16:59:34 +0100

    error handling

--- src/Components/DisplayWeather.js
+++ src/Components/DisplayWeather.js
@@ -1,10 +1,44 @@
+import { useCallback, useEffect, useState } from "react";
+
 function DisplayWeather(props) {
-	console.log('test!');
+	const [display, setDisplay] = useState({});
+	const [loading, setLoading] = useState(null);
+	const [error, setError] = useState(null);
+
+	const fetchWeather = async (query) => {
+		setLoading(true);
+		const appid = "599f9ab00f5ffd6eeb1a6bf54606a714";
+		try {
+			const response = await fetch(
+				`http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appid}`
+				);
+	
+				if (!response.ok) {
+					setError(true)
+					throw new Error('erororororo!')
+				}
+				const data = await response.json();
+				console.log(data);
+	
+				setDisplay({
+					city: data.name,
+					weather: data.weather[0].main,
+					temp: `${(data.main.temp-272).toFixed(0)}°C`,
+				});
+
+		} catch (error) {
+			console.log(error);
+		}
+
+		};
+
+		useEffect(() => fetchWeather(props.get), [props.get])
+
 	return (
 		<div>
-			<div>{props.show.city}</div>
-			<div>{props.show.weather}</div>
-			<div>{props.show.temperature}</div>
+			<div>{display.city}</div>
+			<div>{display.weather}</div>
+			<div>{display.temp}</div>
 		</div>
 	);
 }
--- src/Components/ShowWeather.js
+++ src/Components/ShowWeather.js
@@ -1,55 +1,17 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import DisplayWeather from "./DisplayWeather";
 import PickLocation from "./PickLocation";
 
 function ShowWeather() {
 	const [location, setLocation] = useState("");
-	const [display, setDisplay] = useState({});
-	const [loading, setLoading] = useState(null);
-	const [error, setError] = useState(null);
-
 	const handleSearch = (location) => {
 		setLocation(location);
 	};
 
-	const fetchWeather = async (query) => {
-		setLoading(true);
-		console.log(query);
-		const appid = "599f9ab00f5ffd6eeb1a6bf54606a714";
-		try {
-			const response = await fetch(
-				`http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appid}`
-			);
-			setLoading(false);
-
-			if (!response.ok) {
-				setError(true);
-				console.log("error");
-				throw new Error("Something went wrong!");
-			}
-
-			const data = await response.json();
-			console.log(data);
-
-			setDisplay({
-				city: data.name,
-				weather: data.weather[0].main,
-				temp: data.main.temp,
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	if (location.length > 0) {
-		console.log("fetch weather ", location);
-		fetchWeather(location);
-	}
-
 	return (
 		<div>
 			<PickLocation onSearch={handleSearch} />
-			{location && <DisplayWeather show={display} />}
+			{location.length>0 && <DisplayWeather get={location} />}
 		</div>
 	);
 }
