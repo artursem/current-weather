d687439ca985efd6ea017e35e64cb97b3853e813
artursem <artur.semeniuk@gmail.com>
2021-11-24 19:42:23 +0100

    add custom hook useRequest

--- src/Components/DisplayWeather.js
+++ src/Components/DisplayWeather.js
@@ -1,64 +1,20 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect } from "react";
+import useRequest from "../hooks/useRequest";
 import classes from "./DisplayWeather.module.css";
 
 function DisplayWeather(props) {
-	const [display, setDisplay] = useState({});
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(false);
+	const { display, loading, error, fetchWeather } = useRequest(props.get);
 
-	const getWeatherByCity = async (query, appid) => {
-		return fetch(
-			`http://api.openweathermap.org/data/2.5/weather?q=${query.city}&appid=${appid}`
-		);
-	};
-
-	const getWeatherByGeolocation = async (query, appid) => {
-		return fetch(
-			`http://api.openweathermap.org/data/2.5/weather?lat=${query.lat}&lon=${query.lon}&appid=${appid}`
-		);
-	};
-
-	const fetchWeather = useCallback(async (query) => {
-		setError(false);
-		const appid = "599f9ab00f5ffd6eeb1a6bf54606a714";
-		try {
-			let response;
-			if (query.method === "city") {
-				setLoading(true);
-				response = await getWeatherByCity(query, appid);
-			} else if (query.method === "geo") {
-				setLoading(true);
-				response = await getWeatherByGeolocation(query, appid);
-			}
-
-			if (!response.ok) {
-				setLoading(false);
-				setError(true);
-				throw new Error("No valid response");
-			}
-			const data = await response.json();
-			console.log(data);
-
-			setLoading(false);
-			setDisplay({
-				city: data.name,
-				weather: data.weather[0].main,
-				temp: `${(data.main.temp - 272).toFixed(0)}°C`,
-				icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	}, []);
-
-	useEffect(() => fetchWeather(props.get), [fetchWeather, props.get]);
+	useEffect(() => {
+		fetchWeather(props.get);
+	}, [fetchWeather, props.get]);
 
 	let show;
 
 	if (loading) {
 		show = <p>Loading...</p>;
 	}
-	if (!loading && !error) {
+	if (display && !loading && !error) {
 		show = (
 			<section className={classes.section}>
 				<h1>{display.city}</h1>
---
+++ src/hooks/useRequest.js
@@ -0,0 +1,46 @@
+import { useState, useCallback } from "react";
+
+const useRequest = () => {
+	const [display, setDisplay] = useState({});
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	const fetchWeather = useCallback(async (query) => {
+		setError(false);
+		const appid = "599f9ab00f5ffd6eeb1a6bf54606a714";
+		try {
+			let response;
+			if (query.method === "city") {
+				setLoading(true);
+				response = await fetch(
+					`http://api.openweathermap.org/data/2.5/weather?q=${query.city}&appid=${appid}`
+				);
+			} else if (query.method === "geo") {
+				setLoading(true);
+				response = await fetch(
+					`http://api.openweathermap.org/data/2.5/weather?lat=${query.lat}&lon=${query.lon}&appid=${appid}`
+				);
+			}
+
+			if (!response.ok) {
+				setLoading(false);
+				setError(true);
+				throw new Error("No valid response");
+			}
+			const data = await response.json();
+
+			setLoading(false);
+			setDisplay({
+				city: data.name,
+				weather: data.weather[0].main,
+				temp: `${(data.main.temp - 272).toFixed(0)}°C`,
+				icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+	return { display, loading, error, fetchWeather };
+};
+
+export default useRequest;
